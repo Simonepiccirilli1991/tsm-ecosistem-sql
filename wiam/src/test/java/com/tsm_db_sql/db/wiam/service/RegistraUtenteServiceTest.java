95ebf52d236105186ef5bb6c89993e06038df08b
SimonePiccirilli <simone.piccirilli91@gmail.com>
2026-04-23 18:04:51 +0200

    implemented test case for delete too

--- wiam/src/test/java/com/tsm_db_sql/db/wiam/service/RegistraUtenteServiceTest.java
+++ wiam/src/test/java/com/tsm_db_sql/db/wiam/service/RegistraUtenteServiceTest.java
@@ -3,6 +3,7 @@ package com.tsm_db_sql.db.wiam.service;
 
 import com.tsm_db_sql.db.wiam.entity.Utente;
 import com.tsm_db_sql.db.wiam.exception.UtenteException;
+import com.tsm_db_sql.db.wiam.model.request.DeleteUtenteRequest;
 import com.tsm_db_sql.db.wiam.model.request.RegistraUtenteRequest;
 import com.tsm_db_sql.db.wiam.repository.UtenteRepository;
 import com.tsm_db_sql.db.wiam.service.utente.RegistraUtenteService;
@@ -68,5 +69,48 @@ public class RegistraUtenteServiceTest {
 
         Assertions.assertEquals("Utente gia registrato con email indicata",except.getMessaggio());
 
+    }
+
+    @Test
+    void registraECancellaUtenteServiceTestOK(){
+
+        var utente = new Utente();
+        utente.setUsername("Ajeje");
+        utente.setNome("Ajeje");
+        utente.setCognome("Brazof");
+        utente.setEmail("asd@gmail.com");
+        utente.setPassword("BeaMerda");
+        utente.setDataRegistrazione(LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+
+        utenteRepository.save(utente);
+
+        var request = new DeleteUtenteRequest("Ajeje","BeaMerda");
+
+        var resp = registraUtenteService.cancellaUtente(request);
+
+        Assertions.assertEquals("Utente cancellato con successo",resp.messaggio());
+
+        Assertions.assertTrue(utenteRepository.findByUsername("Ajeje").isEmpty());
+    }
+
+    @Test
+    void registraECancellaUtenteServiceTestKO(){
+
+        var utente = new Utente();
+        utente.setUsername("Ajeje");
+        utente.setNome("Ajeje");
+        utente.setCognome("Brazof");
+        utente.setEmail("asd@gmail.com");
+        utente.setPassword("BeaMerda");
+        utente.setDataRegistrazione(LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+
+        utenteRepository.save(utente);
+
+        var request = new DeleteUtenteRequest("Ajeje","BeaMerda123");
+
+        var exception = Assertions.assertThrows(UtenteException.class, () -> registraUtenteService.cancellaUtente(request));
+
+        Assertions.assertEquals("Passord don't match for delete utente",exception.getMessaggio());
+        Assertions.assertTrue(utenteRepository.findByUsername("Ajeje").isPresent());
     }
 }
